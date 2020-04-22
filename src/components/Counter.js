194b3e3a4a8ba1ab7ff64bb340fb9efba36306aa
Emily Déat <emily.deat@gmail.com>
2020-04-22 11:28:34 +0200

    use hook

---
+++ src/components/Counter.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  // Déclare une nouvelle variable d'état, qu’on va appeler « count »
+  // Déclare une fonction setCount qui permet de modifier la valeur de count
+  const [count, setCount] = useState(0);
+
+  const handleChange = (event) => {
+    setCount(Number(event.target.value))
+  }
+
+  return (
+    <div>
+      <label>Saisie un nombre</label>
+      <input type="number" onChange={handleChange}></input>
+      <p>Le compteur est  à : {count} </p>
+      <button onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -1
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
